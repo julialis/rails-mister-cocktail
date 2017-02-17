5e3ab7480d6b7fefdf098f36108b3ba49e7f4a55
Julia Lissague <julia.lissague@gmail.com>
2017-02-17 22:49:15 +0100

    update

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,8 @@
 class Cocktail < ApplicationRecord
   has_many :doses
   has_many :ingredients, through: :doses
+  mount_uploader :photo, PhotoUploader
+
   # validates :name, uniqueness: true, presence: true
   # attention le dernier validates n'était pas fait dans le master code
   # When you delete a cocktail, you should delete associated doses
